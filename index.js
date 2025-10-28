1b3377a70569a1c460613ae56cdfd3ce7ef42d30
tsol <tsol@nelson-atkins.org>
2025-10-28 10:57:00 -0500

    Current benchmark

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 window.addEventListener('DOMContentLoaded', () => {
-        document.querySelectorAll('nj-image-gallery').forEach(gallery => {
-          gallery.addEventListener('change', (e) => {
-            console.log('Gallery ID:', gallery.id, 'Selected image:', e.detail.value);
-            // You can also do something specific for each gallery here
-          });
+    document.querySelectorAll('nj-image-gallery').forEach(gallery => {
+        gallery.addEventListener('change', (e) => {
+        console.log('Gallery ID:', gallery.id, 'Selected image:', e.detail.value);
+        // You can also do something specific for each gallery here
         });
-      });
+    });
+    document.querySelectorAll('wa-tab-group').forEach(gallery => {
+        gallery.addEventListener('wa-tab-show', (e) => {
+        console.log('Gallery ID:', gallery.id, 'Selected image:', e.detail.name);
+        // You can also do something specific for each gallery here
+        });
+    });
+});
+
+
+function showHome() {
+    
+}
+
+function showGallery(gallery) {
+
+}
+
+function showDetails(imageId) {
+    
+}
